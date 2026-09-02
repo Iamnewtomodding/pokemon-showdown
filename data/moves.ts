1f62206b3c01445eee80156e4d50d5004c044a99
Iamnewtomodding <kunduaditya0206@gmail.com>
2026-09-02 13:13:47 +0530

    Increase base power of Nihil Light from 100 to 200

--- data/moves.ts
+++ data/moves.ts
@@ -12756,7 +12756,7 @@ export const Moves: import('../sim/dex-moves').MoveDataTable = {
 	nihillight: {
 		num: 920,
 		accuracy: 100,
-		basePower: 100,
+		basePower: 200,
 		category: "Special",
 		isNonstandard: "Future",
 		name: "Nihil Light",
